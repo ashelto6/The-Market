07f191a4354f3f4eb343705581cfb4be0fa4fe44
ashelto6 <60279886+ashelto6@users.noreply.github.com>
2021-02-05 04:19:47 -0500

    TDA API config

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template, jsonify
+from tda import auth, client
 import json
+import config
+
+try:
+    c = auth.client_from_token_file(config.token_path, config.api_key)
+except FileNotFoundError:
+    from selenium import webdriver
+    with webdriver.Chrome(executable_path='/mnt/c/Users/ajshe/Desktop/projects/unJumble/chromedriver.exe') as driver:
+        c = auth.client_from_login_flow(
+            driver, config.api_key, config.redirect_uri, config.token_path)
 
 app = Flask(__name__)
 
