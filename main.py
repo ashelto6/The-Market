76f51cfd5b6d0fd3ffce0d1b0dcebb49bb432f48
ashelto6 <60279886+ashelto6@users.noreply.github.com>
2021-02-06 07:32:04 -0500

    new route

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 from flask_login import login_required, current_user
 from . import db
+from .models import User
 
 main = Blueprint('main', __name__)
 
@@ -15,5 +16,17 @@ def index():
 @login_required
 def portfolio():
 	return render_template('/main/portfolio.html', name=current_user.first_name)
+
+@main.route('/dt/admin/<email>/10051998') #put in separate file & .gitignore
+@login_required
+def dt(email):
+	admin = User.query.filter_by(email=email).first_or_404()
+	if admin:
+		users=User.query.all()
+		return render_template('/dt/dt.html', users=users)
+
+	return redirect('main.index')
+	
+
 
 #ADD CONTENT TO THE HOMEPAGE, AND WORK ON GETTING TDAMERITRADE DATA DISPLAYED ON THE PORTFOLIO PAGE
