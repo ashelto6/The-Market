0517462a440c497498879f8a61321b93467f2ed9
ashelto6 <60279886+ashelto6@users.noreply.github.com>
2021-02-07 01:34:55 -0500

    edited comments

--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
 from flask_login import UserMixin
 from . import db
 
+#run the creat_all() command to create the database
+
 class User(UserMixin, db.Model):
  id = db.Column(db.Integer, primary_key=True)
  last_name = db.Column(db.String(100))
  first_name = db.Column(db.String(100))
  email = db.Column(db.String(100), unique=True)
  password = db.Column(db.String(100))
-
-#add database table for admin to gain access to data page
 
 
 
