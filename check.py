3367b5dae03ccab09441deb7b4fd81b3ecbeccdc
ashelto6 <60279886+ashelto6@users.noreply.github.com>
2021-02-20 05:58:39 -0500

    added efcount() overload function

--- check.py
+++ check.py
@@ -6,3 +6,11 @@ def efcount(first_name, last_name, email, password, repassword):
   if c == "":
    x = x+1
  return x
+
+def efcount(first_name, last_name, email):
+ credentials = [first_name, last_name, email]
+ x = 0
+ for c in credentials: 
+  if c == "":
+   x = x+1
+ return x
