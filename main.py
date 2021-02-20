dab3f1ae7a6d748827b3251aaa5b8c5792db4347
ashelto6 <60279886+ashelto6@users.noreply.github.com>
2021-02-20 05:59:26 -0500

    added settings route & editprof subroute

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, redirect, jsonify
+from flask import Blueprint, render_template, redirect, jsonify, request, url_for,flash
 from flask_login import login_required, current_user
 from . import db, TDSession
 from .models import User
+from .check import efcount
 from dotenv import load_dotenv
 import os, json, requests
 
@@ -46,3 +47,43 @@ def portfolio():
 	TDSession.login()
 	data = TDSession.get_accounts(account='all', fields=['positions'])
 	return render_template('/main/portfolio.html', name=current_user.first_name, data=data)
+
+#settings pages route
+@main.route('/settings')
+@login_required
+def settings():
+ user=User.query.filter_by(email = current_user.email).first()
+ 
+ return render_template('/main/settings.html', current_user=user)
+
+@main.route('/settings/edit_profile', methods=['POST', 'GET'])
+@login_required
+def edit_profile():
+ 
+ if request.method == 'GET':
+  return redirect(url_for('main.settings'))
+ 
+ first_name=request.form.get('first name')
+ last_name = request.form.get('last name')
+ email = request.form.get('email')
+ user = User.query.filter_by(email=email).first()
+ 
+ if first_name == current_user.first_name and last_name == current_user.last_name and email == current_user.email:
+  return redirect(url_for('main.settings'))
+ 
+ if email != current_user.email and user:
+  flash('That email is taken.')
+  return redirect(url_for('main.settings'))
+ 
+ empty_fields = efcount(first_name, last_name, email)
+ if empty_fields > 0:
+  flash('Please enter a value for each field.')
+  return redirect(url_for('main.settings'))
+ 
+ current_user.first_name = first_name
+ current_user.last_name = last_name
+ current_user.email = email.lower()
+ db.session.commit()
+ 
+ flash('Account Successfully Updated!')
+ return redirect(url_for('main.settings'))
