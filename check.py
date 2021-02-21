7b39739cf6d7250c0abcc018e728fe96e2c67ca7
ashelto6 <60279886+ashelto6@users.noreply.github.com>
2021-02-21 12:50:32 -0500

    added a check for all-spaces in field entries

--- check.py
+++ check.py
@@ -1,16 +1,18 @@
 
-def efcount(field1, field2, field3, field4, field5): #counts how many of 5 fields are empty - return int
- credentials = [field1, field2, field3, field4, field5]
- x = 0
- for c in credentials: 
-  if c == "":
-   x = x+1
- return x
+def ef5count(field1, field2, field3, field4, field5): #counts how many of 5 fields are empty - return int
+  credentials = [field1, field2, field3, field4, field5]
+  x = 0
+  for c in credentials: 
+    c=c.strip()
+    if c == "":
+      x = x+1
+  return x
 
-def efcount(field1, field2, field3):
- credentials = [field1, field2, field3]
- x = 0
- for c in credentials: 
-  if c == "":
-   x = x+1
- return x
+def ef3count(field1, field2, field3):
+  credentials = [field1, field2, field3]
+  x = 0
+  for c in credentials:
+    c=c.strip()  
+    if c == "":
+      x = x+1
+  return x
