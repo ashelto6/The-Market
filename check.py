cb1c4e09b2d3dfcebcbec47a5e361dcacc8e4598
ashelto6 <60279886+ashelto6@users.noreply.github.com>
2021-02-22 01:18:32 -0500

    added comments to check.py

--- check.py
+++ check.py
@@ -8,7 +8,7 @@ def ef5count(field1, field2, field3, field4, field5): #counts how many of 5 fiel
       x = x+1
   return x
 
-def ef3count(field1, field2, field3):
+def ef3count(field1, field2, field3): #counts how many of 3 fields are empty - return int
   credentials = [field1, field2, field3]
   x = 0
   for c in credentials:
